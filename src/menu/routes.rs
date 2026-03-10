7273cef3d6cb228da711571e55d065dfb70480be
Shawket Ibrahim <shawket.4@icloud.com>
2026-03-10 22:21:41 +0200

    Fixed Menu Module

--- src/menu/routes.rs
+++ src/menu/routes.rs
@@ -1,35 +1,41 @@
 use actix_web::web;
-use super::handlers::*;
+use crate::{auth::middleware::JwtMiddleware, menu::handlers::*};
 
 pub fn configure(cfg: &mut web::ServiceConfig) {
     cfg
         // Categories
-        .route("/categories",     web::get().to(list_categories))
-        .route("/categories",     web::post().to(create_category))
-        .route("/categories/{id}", web::patch().to(update_category))
-        .route("/categories/{id}", web::delete().to(delete_category))
-
-        // Menu items
-        .route("/menu-items",      web::get().to(list_menu_items))
-        .route("/menu-items",      web::post().to(create_menu_item))
-        .route("/menu-items/{id}", web::get().to(get_menu_item))
-        .route("/menu-items/{id}", web::patch().to(update_menu_item))
-        .route("/menu-items/{id}", web::delete().to(delete_menu_item))
-
+        .service(
+            web::scope("/categories")
+                .wrap(JwtMiddleware)
+                .route("",      web::get().to(list_categories))
+                .route("",      web::post().to(create_category))
+                .route("/{id}", web::patch().to(update_category))
+                .route("/{id}", web::delete().to(delete_category)),
+        )
+        // Menu items + option groups/items nested under them
+        .service(
+            web::scope("/menu-items")
+                .wrap(JwtMiddleware)
+                .route("",      web::get().to(list_menu_items))
+                .route("",      web::post().to(create_menu_item))
+                .route("/{id}", web::get().to(get_menu_item))
+                .route("/{id}", web::patch().to(update_menu_item))
+                .route("/{id}", web::delete().to(delete_menu_item))
+                .route("/{id}/option-groups",                               web::get().to(list_option_groups))
+                .route("/{id}/option-groups",                               web::post().to(create_option_group))
+                .route("/{item_id}/option-groups/{group_id}",               web::patch().to(update_option_group))
+                .route("/{item_id}/option-groups/{group_id}",               web::delete().to(delete_option_group))
+                .route("/{item_id}/option-groups/{group_id}/items",         web::post().to(add_option_item))
+                .route("/{item_id}/option-groups/{group_id}/items/{oi_id}", web::patch().to(update_option_item))
+                .route("/{item_id}/option-groups/{group_id}/items/{oi_id}", web::delete().to(delete_option_item)),
+        )
         // Addon items
-        .route("/addon-items",      web::get().to(list_addon_items))
-        .route("/addon-items",      web::post().to(create_addon_item))
-        .route("/addon-items/{id}", web::patch().to(update_addon_item))
-        .route("/addon-items/{id}", web::delete().to(delete_addon_item))
-
-        // Drink option groups
-        .route("/menu-items/{id}/option-groups",                         web::get().to(list_option_groups))
-        .route("/menu-items/{id}/option-groups",                         web::post().to(create_option_group))
-        .route("/menu-items/{item_id}/option-groups/{group_id}",         web::patch().to(update_option_group))
-        .route("/menu-items/{item_id}/option-groups/{group_id}",         web::delete().to(delete_option_group))
-
-        // Drink option items
-        .route("/menu-items/{item_id}/option-groups/{group_id}/items",            web::post().to(add_option_item))
-        .route("/menu-items/{item_id}/option-groups/{group_id}/items/{oi_id}",    web::patch().to(update_option_item))
-        .route("/menu-items/{item_id}/option-groups/{group_id}/items/{oi_id}",    web::delete().to(delete_option_item));
+        .service(
+            web::scope("/addon-items")
+                .wrap(JwtMiddleware)
+                .route("",      web::get().to(list_addon_items))
+                .route("",      web::post().to(create_addon_item))
+                .route("/{id}", web::patch().to(update_addon_item))
+                .route("/{id}", web::delete().to(delete_addon_item)),
+        );
 }
